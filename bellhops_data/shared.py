adc6de844196b9ff9bd5803109ed1eccb1a33f03
Naveen K L <naveen.lekkalapudi@getbellhops.com>
2018-09-06 11:31:42 -0400

    Changing names to database

--- bellhops_data/shared.py
+++ bellhops_data/shared.py
@@ -4,13 +4,13 @@ import psycopg2
 from datetime import timedelta, datetime, date, time
 from sqlalchemy import create_engine
 
-def table_frame_equivalence(schema_name, table_name, gospel_dataframe, output_dataframe, psycopg_connection, postgres_column_type):
+def table_frame_equivalence(schema_name, table_name, database_dataframe, output_dataframe, psycopg_connection, postgres_column_type):
     '''Enter a postgres schema name and table name followed by a pandas dataframe of the table and output dataframe.
     Additionally, a psycopg2.connect() object needs to be provided and the column type for the postgres database table.
     This will resolve differences between the table and dataframe.'''
-    if list(gospel_dataframe.columns) != list(output_dataframe.columns):
-        database_missing = list(output_dataframe.columns.difference(gospel_dataframe.columns))
-        dataframe_missing = list(gospel_dataframe.columns.difference(output_dataframe.columns))
+    if list(database_dataframe.columns) != list(output_dataframe.columns):
+        database_missing = list(output_dataframe.columns.difference(database_dataframe.columns))
+        dataframe_missing = list(database_dataframe.columns.difference(output_dataframe.columns))
         with psycopg_connection as conn:
             cur = conn.cursor()
             for column in database_missing:
